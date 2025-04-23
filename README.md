20bf2005ca7e31f798481996cd44a3dbb684783d
BGNS <igorcarrasco_20@hotmail.com>
2025-04-23 16:56:19 -0300

    Sistema de invencibilidade e vida

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 Ideia pós tutorial:
 
 // jogador tem 3 vidas. A cada vez que ele pega uma maçã, tem uma chance de 1 em 50 de aparecer um coração pra recuperar vida. Cada spike tira 20 pontos e 0.5 coração. Ao final, o score do player vai para um ranking.
+
+Quando o boneco passar a borada, ele retorna pelo outro lado. Se sair pela direita aparece na esquerda e por ai vai.
--- game.js
+++ game.js
@@ -6,9 +6,10 @@ class mainScene {
         // this method is called once at the beginning
         // It will load all the assets, like sprites and sounds
         // Parameters: name of the sprite, path of the image
-        this.load.image('ghost', 'assets/ghost.png');
-        this.load.image('apple', 'assets/apple.png');
-        this.load.image('spik', 'assets/spike.png');
+        this.load.image('ghost', 'assets/ghost.png'); // ghost
+        this.load.image('apple', 'assets/apple.png'); // apple
+        this.load.image('spik', 'assets/spike.png'); // spike
+        this.load.image('heart', 'assets/heart.png'); // vidas
     };
 
     create() {
@@ -33,10 +34,28 @@ class mainScene {
         // To tell Phaser to use the keys input
         this.arrow = this.input.keyboard.createCursorKeys();
 
-        // Criar grupo de spikes
+        // Criar grupo de spikes e vidas
         this.spikes = this.physics.add.group();
-        // Detectar colisão com spike
-        this.physics.add.overlap(this.player, this.spikes, this.hurt, null, this);
+        this.hearts = this.physics.add.group();
+
+        // colisão
+        this.physics.add.overlap(this.player, this.spikes, this.hurt, null, this); // spike
+        this.physics.add.overlap(this.player, this.hearts, this.collectHeart, null, this); // vidas
+
+        // Cria invencibilidade
+        this.invincible = false;
+
+        // Mecanica de vidas
+        this.lives = 3;
+        this.maxLives = 5;
+        this.lifeIcons = [];
+
+        for(let i = 0; i < this.lives; i++) {
+            let icon = this.add.image(30 + i * 20, 60, 'heart');
+            icon.setScale(0.5);
+            this.lifeIcons.push(icon);
+        }
+
     };
 
     update() {
@@ -48,10 +67,10 @@ class mainScene {
             // call the new hit() method
             this.hit();
         }
-        if (this.physics.overlap(this.player, this.spik)) {
-            // call the new spike() method
-            this.hurt();
-        }
+        // if (this.physics.overlap(this.player, this.spik)) {
+        //     // call the new spike() method
+        //     this.hurt();
+        // }
 
 
         // Handle horizontal movements
@@ -100,16 +119,35 @@ class mainScene {
             Phaser.Math.Between(100, 300),
             'spik'
         );
+
+        // 1% de chance de criar um coração
+        if (Phaser.Math.Between(1, 15) === 1) {
+            const heart = this.hearts.create(
+                Phaser.Math.Between(100,600),
+                Phaser.Math.Between(100,300),
+                'heart'
+            );
+        }
     }
 
     hurt(player, spike) {
     //    // change the position x and y of the coin randomly
     //    this.spik.x = Phaser.Math.Between(100, 600);
     //    this.spik.y = Phaser.Math.Between(100, 300);
-       
-        // Decrement the score by 20
-        this.score -= 20;
+        // Verifica a invencibilidade
+        if (this.invincible) return; // Se estiver invencível, então ignora o hit
+
+        this.invincible = true; // como vc tomou hit, ativa a invencibilidade
+
+        // Diminui Score e Vida ao levar hit
+        this.score -= 5;
         if(this.score < 0) { this.score = 0 };
+        this.lives -= 1;
+        if(this.lives <= 0) { // encerra o jogo se a vida chegar a 0
+            alert("GAME OVER");
+            this.scene.restart();
+        }
+        this.updateLifeIcons();
 
         // Display the updated score on the screen
         this.scoreText.setText('score: ' + this.score);
@@ -127,6 +165,33 @@ class mainScene {
         });
         // remove a spike que encostou da tela
         spike.destroy();
+
+        // Depois de 1s, remove a invencibilidade
+        this.time.delayedCall(500, () => {
+            this.invincible = false;
+        });
+    }
+
+    collectHeart(player, heart) {
+        heart.destroy(); 
+
+        if(this.lives < this.maxLives) {
+            this.lives += 1;
+            this.updateLifeIcons();
+        }
+    }
+
+    updateLifeIcons() {
+        // remove todos os ícones visuais
+        this.lifeIcons.forEach(icon => icon.destroy());
+        this.lifeIcons = [];
+
+        // adiciona conforme a quantidade de vidas atual
+        for(let i =0; i < this.lives; i++) {
+            let icon = this.add.image(30 + i * 20, 60, 'heart');
+            icon.setScale(0.5);
+            this.lifeIcons.push(icon); 
+        }
     }
 }
 
